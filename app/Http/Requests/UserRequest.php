53b29d2ad19dc1864f8f8d1c60ed64a101941251
sinyadal <v0xe7y2cl@protonmail.com>
2018-03-11 18:15:23 +0800

    Create custom messages

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            // 'variablename.validatorname' => 'Custom message'
+            'name.required' => 'A name is required, bro',
+            'email.email' => 'Cmonnn mate, that is not an email address'
+        ];
+    }
+}
